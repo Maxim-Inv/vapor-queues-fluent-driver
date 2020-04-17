69ca3d2217b5b66ff67c2adaae78b5efe0aa570d
Maxim Anisimov <maxim@invoodoo.com>
2020-04-17 14:31:03 +0300

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,22 +16,19 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.1"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-rc.2"),
-        .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/queues.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "QueuesFluentDriver",
             dependencies: [
-                "Fluent",
-                "SQLKit",
-                "Queues",
-                "Vapor"
-            ],
-            path: "Sources"
-        )
-        /*.testTarget(
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "Queues", package: "queues")
+            ]
+        ),
+        .testTarget(
             name: "QueuesFluentDriverTests",
-            dependencies: ["QueuesFluentDriver"]),*/
+            dependencies: ["QueuesFluentDriver"]),
     ]
 )
